ffc63c3d4f17ae45a857f7c168fda7488536617b
fadli131 <fadliaryadinata011@gmail.com>
2024-03-31 19:29:49 +0700

    update code

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,2 @@
 export { default as ButtonCustom } from './ButtonCustom'
-export { default as Header } from './Header'
 export { default as ProfilePicture} from './ProfilePicture'
--- src/containers/ProfileContainer/index.tsx
+++ src/containers/ProfileContainer/index.tsx
@@ -1,12 +1,13 @@
 import { Box, Grid } from "@mui/material";
 import styles from './ProfileContainer.module.scss'
-import { Header, ProfilePicture } from "../../components";
+import { ProfilePicture } from "../../components"
+import { NavBar } from "../../layouts";
 
 export default function ProfileContainer() {
   return (
     <>
       <Box>
-        <Header />
+      <NavBar />
         <Box style={{ maxHeight: '100vh', overflowY: 'auto', scrollbarWidth: 'none' }} className={styles.root}>
             <Grid xs={6} md={4}> 
               <Box className={styles.profile} style={{ marginTop: '6rem' }}>
