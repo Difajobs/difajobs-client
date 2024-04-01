f79b30a1bab2181859903fe54faa4bfe824bf6aa
rz0u <rayhan.zou98@gmail.com>
2024-04-01 20:43:25 +0700

    feat: fetchApi register boiler

--- src/utils/fetchApi.ts
+++ src/utils/fetchApi.ts
@@ -5,7 +5,32 @@ interface login {
   value2: string;
 }
 
+interface register {
+  value: string;
+  value2: string;
+  value3: string;
+}
+
 export const login = async (value: login) => {
+  try {
+    const response = await fetch(API_URL + "endpoint", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(value),
+    });
+    if (!response.ok) {
+      throw response;
+    }
+    return response;
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+export const register = async (value: register) => {
   try {
     const response = await fetch(API_URL + "endpoint", {
       method: "POST",
