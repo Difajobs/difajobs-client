29d07dcaba914f81d7a48da9834ea6e316b24460
alyuza <alyuza.sat@gmail.com>
2024-04-02 14:52:08 +0700

    fix: change route

--- src/pages/DashboardPage/index.tsx
+++ src/pages/DashboardPage/index.tsx
@@ -1,14 +1,17 @@
 import { Box } from "@mui/material";
 import { DashboardContainer } from "../../containers";
 import styles from './DashboardPage.module.scss'
-import { NavBar } from "../../layouts";
+import { Footer, NavBar } from "../../layouts";
+import AuthChecker from "../../utils/authChecker";
 
 export default function DashboardPage() {
   return (
     <>
+      <AuthChecker />
       <Box className={styles.dashboardPage}>
         <NavBar />
         <DashboardContainer />
+        <Footer />
       </Box>
     </>
   )
