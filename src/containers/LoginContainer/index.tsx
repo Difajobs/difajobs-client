c9f71a8dd75a3bf41dd4d4857e314cd0e3a7e3e3
alyuza <alyuza.sat@gmail.com>
2024-04-15 16:50:31 +0700

    refactor: add login form component

--- src/containers/LoginContainer/index.tsx
+++ src/containers/LoginContainer/index.tsx
@@ -1,89 +1,15 @@
-import { Box, Button, Divider, TextField, Typography } from "@mui/material";
-import { Link, useNavigate } from 'react-router-dom';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import CircularProgress from '@mui/material/CircularProgress';
+import { Box, Typography } from "@mui/material";
 import styles from './LoginContainer.module.scss';
-import difaJobsLogo from '../../assets/images/difajobs-dark.webp';
-import googleLogo from '../../assets/images/google.svg';
-import { login } from '../../utils/fetchApi';
+import LoginForm from "../../components/LoginForm";
 
 export default function LoginContainer() {
-  const navigate = useNavigate();
-
-  const formik = useFormik({
-    initialValues: {
-      email: '',
-      password: ''
-    },
-    validationSchema: Yup.object({
-      email: Yup.string().email('Invalid email address').required('Email is required'),
-      password: Yup.string().required('Password is required')
-    }),
-    onSubmit: async (values, { setSubmitting, setFieldError }) => {
-      try {
-        const response = await login(values);
-        localStorage.setItem('token', response.token);
-        navigate('/dashboard');
-      } catch (error) {
-        setFieldError('email', 'Invalid email or password');
-        setFieldError('password', 'Invalid email or password');
-      } finally {
-        setSubmitting(false);
-      }
-    }
-  });
-
   return (
     <Box className={styles.loginContainer}>
       <Box className={styles.logoBox}>
         <Typography className={styles.logoText}>DIFA JOBS</Typography>
       </Box>
       <Box className={styles.formContainer}>
-        <img src={difaJobsLogo} alt="Difa Jobs Logo" className={styles.difaJobsLogo} />
-        <form onSubmit={formik.handleSubmit} className={styles.inputForm} >
-          <Box className={styles.inputBox}>
-            <Typography className={styles.inputLabel}>Email</Typography>
-            <TextField
-              autoComplete="off"
-              className={styles.inputField}
-              variant="outlined"
-              type="email"
-              {...formik.getFieldProps('email')}
-              InputProps={{ style: { borderRadius: '20px', height: '52px', padding: '0px 5px' } }}
-            />
-            {formik.touched.email && formik.errors.email && (
-              <Typography className={styles.errorMsg}>{formik.errors.email}</Typography>
-            )}
-          </Box>
-          <Box className={styles.inputBox}>
-            <Typography className={styles.inputLabel}>Password</Typography>
-            <TextField
-              autoComplete="off"
-              className={styles.inputField}
-              variant="outlined"
-              type="password"
-              {...formik.getFieldProps('password')}
-              InputProps={{ style: { borderRadius: '20px', height: '52px', padding: '0px 5px' } }}
-            />
-            {formik.touched.password && formik.errors.password && (
-              <Typography className={styles.errorMsg}>{formik.errors.password}</Typography>
-            )}
-          </Box>
-          <Box className={styles.buttonBox}>
-            <Button className={styles.button} type="submit" disabled={formik.isSubmitting}>
-              {formik.isSubmitting ? <CircularProgress size={24} color="inherit" /> : 'Masuk'}
-            </Button>
-            <Divider className={styles.divider} />
-            <Button className={styles.googleButton}>
-              <img src={googleLogo} alt="Google Logo" className={styles.googleLogo} />
-              <Typography className={styles.googleText}>Masuk dengan Google</Typography>
-            </Button>
-            <Typography className={styles.registerText}>
-              Belum punya akun? <Link to="/register" className={styles.registerLink}>Daftar disini</Link>
-            </Typography>
-          </Box>
-        </form>
+        <LoginForm />
       </Box>
     </Box>
   );
