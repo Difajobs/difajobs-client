69bc1de62e9104e724ab18d3d0abb157160d2c8b
fadli131 <fadliaryadinata011@gmail.com>
2024-04-16 11:24:53 +0700

    fix responsive

--- src/components/ProfileBio/Disabilitas/index.tsx
+++ src/components/ProfileBio/Disabilitas/index.tsx
@@ -12,7 +12,7 @@ const Disabilitas: React.FC = () => {
       <Box className={styles.formContainer}>
         <Box>
           <Typography className={styles.title}>
-            Keahlian & Sertifikasi
+            Disabilitas dan Hambatan
           </Typography>
         </Box>
         <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
@@ -20,26 +20,26 @@ const Disabilitas: React.FC = () => {
             <Box className={styles.keahlian}>
               <Box>
                   <Typography className={styles.inputLabel}>
-                    Ringkasan Pribadi
+                    Disabilitas
                   </Typography>
               </Box>
               <FormGroup className={styles.ringkasanpribadi}>
-                <FormControlLabel control={<Checkbox defaultChecked />} label="Keterbatasan Mobilitas" className={styles.label} />
-                <FormControlLabel control={<Checkbox defaultChecked />} label="Keterbatasan Penglihatan" />
-                <FormControlLabel control={<Checkbox defaultChecked />} label="Keterbatasan Pendengaran" />
+                <FormControlLabel className={styles.checkbutton} control={<Checkbox defaultChecked />} label="Keterbatasan Mobilitas" />
+                <FormControlLabel className={styles.checkbutton} control={<Checkbox defaultChecked />} label="Keterbatasan Penglihatan" />
+                <FormControlLabel className={styles.checkbutton} control={<Checkbox defaultChecked />} label="Keterbatasan Pendengaran" />
               </FormGroup>
             </Box>
           </Grid>
           <Grid item xs={6}>
             <Box className={styles.keahlian}>
               <Typography className={styles.inputLabel}>
-                Disabilitas & Hambatan
+                Hambatan
               </Typography>
             </Box>
             <FormGroup className={styles.ringkasanpribadi}>
-                <FormControlLabel control={<Checkbox defaultChecked />} label="Kehilangan Salah Satu Lengan" />
-                <FormControlLabel control={<Checkbox defaultChecked />} label="Penglihatan Terbatas" />
-                <FormControlLabel control={<Checkbox defaultChecked />} label="Tinnitus" />
+                <FormControlLabel className={styles.checkbutton} control={<Checkbox defaultChecked />} label="Kehilangan Salah Satu Lengan" />
+                <FormControlLabel className={styles.checkbutton} control={<Checkbox defaultChecked />} label="Penglihatan Terbatas" />
+                <FormControlLabel className={styles.checkbutton} control={<Checkbox defaultChecked />} label="Tinnitus" />
             </FormGroup>
           </Grid>
         </Grid>
