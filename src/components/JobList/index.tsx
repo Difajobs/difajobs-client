e30410b5d0eee075e63fd7a7aa9a8d2d0d25cf09
alyuza <alyuza.sat@gmail.com>
2024-04-19 13:16:45 +0700

    refactoring: separate job list, pagination, profile dashboard and search box

--- src/components/JobList/index.tsx
+++ src/components/JobList/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Avatar, Box, Divider, Typography } from "@mui/material";
 import HearingDisabledIcon from "@mui/icons-material/HearingDisabled";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
@@ -6,25 +7,29 @@ import PsychologyIcon from "@mui/icons-material/Psychology";
 import MaleIcon from '@mui/icons-material/Male';
 import FemaleIcon from '@mui/icons-material/Female';
 import styles from './JobList.module.scss';
-import Pagination from '@mui/material/Pagination';
-import React, { useState } from 'react';
+import dummyData from './dummyData';
 
-export default function JobListComponent() {
-  const [currentPage, setCurrentPage] = useState(1);
-  const itemsPerPage = 5; // Change this value to adjust the number of items per page
+interface Job {
+  name: string;
+  role: string;
+  company: string;
+  location: string;
+  time: string;
+  description: string;
+}
+
+interface JobListComponentProps {
+  startIndex: number;
+  endIndex: number;
+}
+
+const JobListComponent: React.FC<JobListComponentProps> = ({ startIndex, endIndex }) => {
   const maxDescriptionLength = 220;
-
-  const handleChangePage = (_event: React.ChangeEvent<unknown>, newPage: number) => {
-    setCurrentPage(newPage);
-  };
-
-  const startIndex = (currentPage - 1) * itemsPerPage;
-  const endIndex = startIndex + itemsPerPage;
 
   return (
     <>
       <Box className={styles.container}>
-        {dummyData.slice(startIndex, endIndex).map((job, index) => (
+        {dummyData.slice(startIndex, endIndex).map((job: Job, index: number) => (
           <Box key={index} className={styles.jobList}>
             <Box className={styles.userInfo}>
               <Box className={styles.avatarInfo}>
@@ -72,89 +77,9 @@ export default function JobListComponent() {
             </Typography>
           </Box>
         ))}
-        <Pagination
-          className={styles.pagination}
-          count={Math.ceil(dummyData.length / itemsPerPage)}
-          page={currentPage}
-          onChange={handleChangePage}
-        />
       </Box>
     </>
-  )
-}
+  );
+};
 
-// Dummy data
-const dummyData = [
-  {
-    name: "John Doe",
-    role: "Software Engineer",
-    company: "ABC Company",
-    location: "New York",
-    time: "2 days ago",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  },
-  {
-    name: "Jane Smith",
-    role: "UI/UX Designer",
-    company: "XYZ Inc.",
-    location: "San Francisco",
-    time: "1 week ago",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  },
-  {
-    name: "Jane Smith",
-    role: "UI/UX Designer",
-    company: "XYZ Inc.",
-    location: "San Francisco",
-    time: "1 week ago",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  },
-  {
-    name: "Jane Smith",
-    role: "UI/UX Designer",
-    company: "XYZ Inc.",
-    location: "San Francisco",
-    time: "1 week ago",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  },
-  {
-    name: "Jane Smith",
-    role: "UI/UX Designer",
-    company: "XYZ Inc.",
-    location: "San Francisco",
-    time: "1 week ago",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  },
-  {
-    name: "Jane Smith",
-    role: "UI/UX Designer",
-    company: "XYZ Inc.",
-    location: "San Francisco",
-    time: "1 week ago",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  },
-  {
-    name: "Jane Smith",
-    role: "UI/UX Designer",
-    company: "XYZ Inc.",
-    location: "San Francisco",
-    time: "1 week ago",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  },
-  {
-    name: "Jane Smith",
-    role: "UI/UX Designer",
-    company: "XYZ Inc.",
-    location: "San Francisco",
-    time: "1 week ago",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  },
-  {
-    name: "Jane Smith",
-    role: "UI/UX Designer",
-    company: "XYZ Inc.",
-    location: "San Francisco",
-    time: "1 week ago",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-  },
-];
+export default JobListComponent;
--- src/containers/DashboardContainer/index.tsx
+++ src/containers/DashboardContainer/index.tsx
@@ -1,16 +1,28 @@
-import { Box, Divider } from "@mui/material"
+import React from 'react';
+import dummyData from '../../components/JobList/dummyData';
+import { CustomPagination, JobListComponent, JobSearchBox, ProfileDashboard } from '../../components';
+import { Box, Divider } from '@mui/material';
 import styles from './DashboardContainer.module.scss'
-import { JobListComponent, JobSearchBox, ProfileDashboard } from "../../components"
 
-export default function DashboardContainer() {
+const JobListContainer: React.FC = () => {
+  const itemsPerPage = 5; // Change this value to adjust the number of items per page
+
   return (
     <>
       <Box className={styles.dashboardContainer}>
         <ProfileDashboard />
-        <Divider className={styles.divider}/>
+        <Divider className={styles.divider} />
         <JobSearchBox />
-        <JobListComponent />
+        <CustomPagination
+          dataLength={dummyData.length}
+          itemsPerPage={itemsPerPage}
+          render={(startIndex, endIndex) => (
+            <JobListComponent startIndex={startIndex} endIndex={endIndex} />
+          )}
+        />
       </Box>
     </>
-  )
-}
+  );
+};
+
+export default JobListContainer;
